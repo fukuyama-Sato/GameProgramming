9ab2f743ecd160e807eab9afac7e3cd487ffa6b6
sato-kazumi <20010507@anabuki-net.ne.jp>
2021-07-09 13:53:41 +0900

    課題22

--- ゲームプログラミング実習 課題/GameProgramming/CXPlayer.cpp
+++ ゲームプログラミング実習 課題/GameProgramming/CXPlayer.cpp
@@ -5,10 +5,20 @@ void CXPlayer::Update(){
 
 	if (CKey::Push('W')){
 		ChangeAnimation(1, true, 60);
-		mPosition.mZ += 0.1f;
+		mPosition = CVector(0.0f, 0.0f, 0.1f)*mMatrix;
 	}
+
 	else{
 		ChangeAnimation(0, true, 60);
 	}
+
+	if (CKey::Push('A')){
+		mRotation.mY += 2;
+	}
+
+	if (CKey::Push('D')){
+		mRotation.mY -= 2;
+	}
+
 	CXCharacter::Update();
 }
